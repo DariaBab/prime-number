30250a4798e1ba00ee6e57001481f63a0f38ba6c
Daria Babchenko <dashiko.tym@gmail.com>
2023-01-05 21:07:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const num = document.querySelector('.number');
+const btn = document.querySelector('a');
+const output = document.querySelector('.output');
+
+
+function primeNumber(num) {
+    num = Number(num);
+    if (num <= 1 || num === isNaN || Number.isInteger(num) === false || num === "" || num === null) {
+        return "Please enter a positive number greater than 1";
+    }
+    
+    if (num === 2) {
+        return 'Congratulations, this is the first prime number!';
+    }
+    if (num % 2 === 0) {
+        return "No, it's a composite number :(";
+    }
+
+    let i = 3;
+    while (i <= Math.sqrt(num)) {
+        if (num % i === 0) {
+            return "No, it's a composite number :(";
+        }
+        i++;
+    }
+    return 'Yes, it is a prime number!';
+}
+
+btn.addEventListener('click', e => {
+
+    output.innerHTML = primeNumber(num.value);
+
+});
